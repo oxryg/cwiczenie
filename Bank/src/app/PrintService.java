81443f05dc37c063638ff48fc79de0f36685de47
oxryg <56882369+oxryg@users.noreply.github.com>
2019-11-24 19:13:36 +0000

    Create PrintService.java

---
+++ Bank/src/app/PrintService.java
@@ -0,0 +1,28 @@
+package app;
+
+import Account.BankAccount;
+import data.Address;
+import data.Balance;
+import data.Person;
+
+public class PrintService {
+
+
+   public void printAcccount(Person owner, Address address, Balance balance){
+       printName(owner);
+       printAddress(address);
+       printBalance(balance);   }
+
+    public void printName (Person person) {
+        System.out.println(person.getName() + " " + person.getSurname() + " PESEL - " + person.getPesel());
+
+    }
+    public void printAddress(Address address) {
+        System.out.println("ul. " + address.getStreet() + " " + address.getHouse() + "/" + address.getApartment());
+        System.out.println(address.getPostcode() + " " + address.getTown());
+    }
+    public void printBalance(Balance balance) {
+        System.out.println(balance.getBalance() + " PLN");
+    }
+}
+
