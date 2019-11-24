0bda0a146c8152b5d6ec917fcc47d2c83bfdb1a9
oxryg <56882369+oxryg@users.noreply.github.com>
2019-11-24 19:15:05 +0000

    Update TestApp.java

--- Bank/src/app/TestApp.java
+++ Bank/src/app/TestApp.java
@@ -9,11 +9,14 @@ public class TestApp {
     public static void main(String[] args) {
 
         Person owner = new Person("Jan", "Kowal");
+        Person owner1 = new Person("Paw", "Kusiak");
         Address address = new Address("Hel", "70-200", "Morska", "13");
+        Address address1 = new Address("Starachowice", "27-200", "Hauke", "3", "38");
         Balance balance = new Balance(15256.50);
-        BankAccount account = new BankAccount(owner,address,balance);
+        Balance balance1 = new Balance(300);
 
-        account.printInfo();
 
-    }
+        PrintService printService = new PrintService();
+        printService.printAcccount(owner, address, balance);
+        printService.printAcccount(owner1, address1, balance1);
 }
