6495ccd21fdd47b69e66742979599ffcc4907c9e
oxryg <56882369+oxryg@users.noreply.github.com>
2019-11-24 19:31:12 +0000

    Update BankAccount.java

--- Bank/src/Account/BankAccount.java
+++ Bank/src/Account/BankAccount.java
@@ -4,16 +4,8 @@ import data.*;
 
 public class BankAccount {
 
-    Person owner = new Person("Jan", "Kowal");
-    Person owner1 = new Person("Paw", "Kusiak");
-    Address address = new Address("Hel", "70-200", "Morska", "13");
-    Address address1 = new Address("Starachowice", "27-200", "Hauke", "3", "38");
-    Balance balance = new Balance(15256.50);
-    Balance balance1 = new Balance(300);
-
+   
     public BankAccount(Person owner, Address address, Balance balance) {
-        this.owner = owner;
-        this.address = address;
-        this.balance = balance;
+       
    }
 }
